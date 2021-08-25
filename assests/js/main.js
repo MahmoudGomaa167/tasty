8702c00c1916ead3e572b09fe8e058583f9c91c8
MahmoudGomaa167 <83142218+MahmoudGomaa167@users.noreply.github.com>
2021-08-25 17:48:57 +0200

    Finished Responsive and working on menu in responsive

--- assests/js/main.js
+++ assests/js/main.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
     // Scroll Smoothly to sections
     for(let link of navLinks){
         let sectionHref = $(link).attr('href');
-        let sectionOffset = Math.ceil($(sectionHref).offset().top);
+        let sectionOffset = Math.ceil($(sectionHref).offset().top + 50);
 
         $(link).click(function(){
             $('body, html').animate({
                 scrollTop: sectionOffset
-            }, 1500);
+            }, 2000);
         });
     }
     
@@ -29,7 +29,7 @@ $(document).ready(function(){
             let sectionHref = $(link).attr('href');
             let sectionOffset = Math.ceil($(sectionHref).offset().top);
 
-            if(windowScrollTop > sectionOffset - 150){
+            if(windowScrollTop > sectionOffset - 100){
                 $(link).addClass('active');
                 $(link).parent().siblings().children().removeClass('active');
             }
@@ -41,9 +41,11 @@ $(document).ready(function(){
         let windowScrollTop = $(window).scrollTop();
         if(windowScrollTop > 100){
             $('.goup').css('opacity', '1');
+            $('nav').css('box-shadow', '0 2px 8px rgba(3, 74, 40, 0.15)')
         }
         else{
             $('.goup').css('opacity', '0');
+            $('nav').css('box-shadow', 'none')
         }
     });
 
@@ -52,7 +54,7 @@ $(document).ready(function(){
         let homeOffset = $('#home').offset().top;
         $('body, html').animate({
             scrollTop: homeOffset
-        }, 1500);
+        }, 2000);
     });
     
 })
